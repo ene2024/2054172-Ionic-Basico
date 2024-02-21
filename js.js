48aba603eda6773bbad7a59cbad1b99edcf43cc1
mmalexei <141696375+mmalexei@users.noreply.github.com>
2024-02-20 20:05:59 -0600

    practica 4

--- js.js
+++ js.js
@@ -9,3 +9,24 @@ function registro(){
     var txt = document.getElementById("texto").value;
     av.textContent += " " + txt;
 }
+
+var articuloInput=document.getElementById("tipo");
+var montoInput=document.getElementById("monto");
+
+
+var articulo = '';
+var monto = 0;
+var total = 0;
+
+ var seccionTicket=document.getElementById('ticket');
+
+function agregar(){
+    articulo = articuloInput.value;
+    monto = parseInt(montoInput.value);
+
+    let elementoTicket = document.createElement('p');
+
+    elementoTicket.innerHTML = articulo + '........ $' + monto;
+
+    seccionTicket.appendChild(elementoTicket)
+}
